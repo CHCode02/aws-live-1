0ec211a4f66417122bb5bec120d4dec0798187dc
CHCode02 <88426426+CHCode02@users.noreply.github.com>
2023-09-25 17:33:27 +0800

    Delete aws-live-main directory

--- aws-live-main/config.py
+++
@@ -1,7 +0,0 @@
-customhost = "employee.cjhemmvcoeku.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "leechenhong-employee"
-customregion = "us-east-1"
-
