14a4dd4d00fe728af6795972d0a716cb5573700a
CHCode02 <88426426+CHCode02@users.noreply.github.com>
2023-09-25 17:33:01 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cjhemmvcoeku.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leechenhong-employee"
+customregion = "us-east-1"
+
